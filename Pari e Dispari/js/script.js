350fe77b952ebbae582601b454f3a5d3c5df0e06
micheleboanno92 <michelebonanno92@gmail.com>
2024-08-17 12:24:54 +0200

    soluzione 1

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -9,9 +9,45 @@ Dichiariamo chi ha vinto.
 
  */
 
-const sceltaUtente = prompt('scegli Pari o Dispari');
-console.log('sceltaUtente', sceltaUtente );
+const userChoice = prompt('scegli p o d');
+console.log('userChoice', userChoice, typeof userChoice );
 
-const numeroUtente = parseInt(prompt('inserisci un numero'));
-console.log('numeroUtente', numeroUtente );
+const userNumber = parseInt(prompt('Scegli un numero tra 1 e 5 (compresi):'));
+console.log('userNumber', userNumber , typeof userNumber);
+
+const pcNumber = generateRandomNumber();
+console.log('pcNumber', pcNumber , typeof pcNumber);
+
+const sum = userNumber + pcNumber;
+console.log('sum', sum , typeof sum);
+
+if ( userChoice == 'p' && sum % 2 == 0 ){
+  alert(' hai vinto ')
+}
+else if ( userChoice == 'd' && !sum % 2 == 0){
+  alert(' hai vinto ')
+}
+else {
+   alert('ha vinto il computer') 
+}
+
+/*
+ Functions
+*/
+
+function generateRandomNumber() {
+    const num =  Math.floor(Math.random() * 5 ) + 1;
+
+    return num;
+}
+
+function  evenOrOdd () {
+    if ( num % 2 == 0 ) {
+        return 'pari';
+    }
+    else {
+        return 'dispari';
+    }
+
+}
 
